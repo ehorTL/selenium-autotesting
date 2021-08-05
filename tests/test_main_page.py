6de1f0fd410eee8d03a7d62c382032c496f62b1e
YEHOR POLISHCHUK <e-dp@ukr.net>
2021-08-05 16:26:48 +0300

    login link presence test added

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -5,9 +5,5 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-
-    # link = "http://selenium1py.pythonanywhere.com/"
-    # browser.get(link)
-    # login_link = browser.find_element_by_css_selector("#login_link")
-    # login_link.click()
+    # page.go_to_login_page()
+    page.should_be_login_link()
