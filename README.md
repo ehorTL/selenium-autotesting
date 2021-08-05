f5ab635a623e155c4d8e59a08d15c46e14000a00
YEHOR POLISHCHUK <e-dp@ukr.net>
2021-08-05 15:26:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Task descriptioin [here](https://stepik.org/lesson/199980/step/6?auth=login&unit=174035)
+
+#### Requirements:
+    - selenium webdriver
+    - pytest
+
+#### Run test example command: `pytest -v --tb=line --language=en tests\test_main_page.py`
---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# Nothing said about cross-browser comapatability in requirements to this task!!!
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Enter language: ru, en, es...")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lang = request.config.getoption('language')
+    options = Options()
+
+    # TODO: input options must be validated
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+
+    browser = webdriver.Chrome(options=options)
+    yield browser
+
+    browser.quit()
